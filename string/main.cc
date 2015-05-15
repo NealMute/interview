c6155fdfde168f905761caa359ccd7939cd3e295
NealMute <mutecaffrey@gmail.com>
2015-05-15 10:52:24 +0800

    修改main.cc，添加test case
    assert断言测试已有的qh::string的接口

--- string/main.cc
+++ string/main.cc
@@ -1,17 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "qh_string.h"
+#include <assert.h>
+#include <string.h>
+#include <iostream>
+#define H_ARRAYSIZE(a) \
+    ((sizeof(a) / sizeof(*(a))) / \
+    static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
 
+#define H_ARRAY_SIZE H_ARRAYSIZE
+
+void test1()
+{
+    using namespace qh;
+    const char* oldstring = "qhstringtestcase1";
+    qh::string qhstr(oldstring);
+    assert(!strcmp(qhstr.data(), oldstring));
+}
+
+void test2()
+{
+    using namespace qh;
+    const char* oldstring = "qhstringtestcase2";
+    qh::string qhstr1(oldstring);
+    qh::string qhstr2(qhstr1);
+    assert(!strcmp(qhstr2.data(), oldstring));
+}
+
+void test3()
+{
+    using namespace qh;
+    qh::string qhstr("qhstringtestcase3");
+    char c = qhstr[5];
+    assert(c=='i');
+}
+
+void test4()
+{
+    using namespace qh;
+    qh::string qhstr("qhstringtestcase4");
+    qhstr[5] = 'Q';
+    assert(!strcmp(qhstr.data(), "qhstrQngtestcase4"));
+}
+
+void test5()
+{
+    using namespace qh;
+    const char* oldstring = "qhstringtestcase5";
+    qh::string qhstr1(oldstring);
+    qh::string qhstr2 = qhstr1;
+    assert(!strcmp(qhstr2.data(), oldstring));
+}
+
+void test6()
+{
+    using namespace qh;
+    const char* oldstring = "qhstringtestcase5";
+    qh::string qhstr1(oldstring,8);
+    assert(!strcmp(qhstr1.data(), "qhstring"));
+}
+
+void test7()
+{
+    using namespace qh;
+    qh::string qhstr1;
+    assert(qhstr1.size() == 0);
+}
 
 int main(int argc, char* argv[])
 {
-    //TODO ÔÚÕâÀïÌí¼Óµ¥Ôª²âÊÔ£¬Ô½¶àÔ½ºÃ£¬´úÂëÂ·¾¶¸²¸ÇÂÊÔ½È«Ô½ºÃ
-    //TODO µ¥Ôª²âÊÔĞ´·¨Çë²Î¿¼INIParserÄÇ¸öÏîÄ¿£¬²»ÒªĞ´Ò»¶Ñprintf£¬ÒªÓÃassert½øĞĞ¶ÏÑÔÅĞ¶Ï¡£
-
+    //TODO åœ¨è¿™é‡Œæ·»åŠ å•å…ƒæµ‹è¯•ï¼Œè¶Šå¤šè¶Šå¥½ï¼Œä»£ç è·¯å¾„è¦†ç›–ç‡è¶Šå…¨è¶Šå¥½
+    //TODO å•å…ƒæµ‹è¯•å†™æ³•è¯·å‚è€ƒINIParseré‚£ä¸ªé¡¹ç›®ï¼Œä¸è¦å†™ä¸€å †printfï¼Œè¦ç”¨assertè¿›è¡Œæ–­è¨€åˆ¤æ–­ã€‚
 #ifdef WIN32
     system("pause");
 #endif
 
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();
+    std::cout<<"All test passed!"<<std::endl;
 	return 0;
 }
-
